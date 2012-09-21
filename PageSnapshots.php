30c03e8bbe57ea9ed625a5e54b73723d1d149934
Vitaliy Filippov <vitalif@yourcmc.ru>
2012-09-21 10:41:23 +0000

    Bug 108732 - Move function to a class; include snapshot parameter into page links

--- PageSnapshots.php
+++ PageSnapshots.php
@@ -26,68 +26,130 @@
  */
 
 $wgExtensionMessagesFiles['PageSnapshots'] = dirname(__FILE__).'/PageSnapshots.i18n.php';
-$wgHooks['PageHistoryLineEnding'][] = 'wfSnapshotHistoryItem';
-$wgHooks['ArticleViewHeader'][] = 'wfSnapshotStart';
-$wgHooks['BeforeParserFetchTemplateAndtitle'][] = 'wfSnapshotTemplateRev';
-$wgHooks['BeforeParserFetchFileAndTitle'][] = 'wfSnapshotFileRev';
+$wgHooks['PageHistoryLineEnding'][] = 'PageSnapshotsExtension::historyItem';
+$wgHooks['BeforeInitialize'][] = 'PageSnapshotsExtension::init';
+$wgHooks['ArticleViewHeader'][] = 'PageSnapshotsExtension::start';
+$wgHooks['BeforeParserFetchTemplateAndtitle'][] = 'PageSnapshotsExtension::templateRev';
+$wgHooks['BeforeParserFetchFileAndTitle'][] = 'PageSnapshotsExtension::fileRev';
+$wgHooks['LinkBegin'][] = 'PageSnapshotsExtension::linkRev';
 
 $egSnapshot = NULL;
 
-function wfSnapshotHistoryItem($pager, &$row, &$s, &$classes)
+class PageSnapshotsExtension
 {
-    $p = strpos($s, ')');
-    if ($p !== false)
-    {
-        $link = $pager->title->getLocalUrl(array('oldid' => $row->rev_id, 'snapshot' => 1));
-        $msg = wfMsg('page-history-snapshot');
-        $s = substr($s, 0, $p) . ' | <a href="'.htmlspecialchars($link).'">'.$msg.'</a>' . substr($s, $p);
-    }
-    return true;
-}
-
-function wfSnapshotStart($article, &$outputDone, &$useParserCache)
-{
-    global $wgRequest, $egSnapshot;
-    if ($article->getOldId() && $wgRequest->getVal('snapshot'))
-    {
-        $egSnapshot = $wgRequest->getVal('snapshot');
-        $egSnapshot = $egSnapshot && $egSnapshot != 1 ? wfTimestampOrNull(TS_MW, $egSnapshot) : NULL;
-        $egSnapshot = $egSnapshot ?: $article->getTimestamp();
-        $useParserCache = false;
-    }
-    return true;
-}
-
-function wfSnapshotTemplateRev($parser, $title, &$skip, &$id)
-{
-    global $egSnapshot;
-    if ($egSnapshot)
+    /**
+     * Get page revision id, actual for $time
+     */
+    static function getRevByTime($title, $time)
     {
         $dbr = wfGetDB(DB_SLAVE);
-        $id = $dbr->selectField(
+        $oldid = $dbr->selectField(
             array('page', 'revision'), 'rev_id', array(
                 'page_id=rev_page',
                 'page_namespace' => $title->getNamespace(),
                 'page_title' => $title->getDBkey(),
-                'rev_timestamp <= \''.wfTimestamp(TS_MW, $egSnapshot).'\'',
+                'rev_timestamp <= \''.wfTimestamp(TS_MW, $time).'\'',
             ), __METHOD__, array('ORDER BY' => 'rev_timestamp DESC', 'LIMIT' => 1)
         );
+        return $oldid;
     }
-    return true;
-}
 
-function wfSnapshotFileRev($parser, $title, &$time, &$sha1, &$descQuery)
-{
-    global $egSnapshot;
-    if ($egSnapshot)
+    /**
+     * Append snapshot links to history items
+     */
+    static function historyItem($pager, &$row, &$s, &$classes)
     {
-        $dbr = wfGetDB(DB_SLAVE);
-        $time = $dbr->selectField(
-            'oldimage', 'oi_timestamp', array(
-                'oi_name' => $title->getDBkey(),
-                'oi_timestamp <= \''.wfTimestamp(TS_MW, $egSnapshot).'\'',
-            ), __METHOD__, array('ORDER BY' => 'oi_timestamp DESC', 'LIMIT' => 1)
-        );
+        $p = strpos($s, ')');
+        if ($p !== false)
+        {
+            $link = $pager->title->getLocalUrl(array('oldid' => $row->rev_id, 'snapshot' => 1));
+            $msg = wfMsg('page-history-snapshot');
+            $s = substr($s, 0, $p) . ' | <a href="'.htmlspecialchars($link).'">'.$msg.'</a>' . substr($s, $p);
+        }
+        return true;
     }
-    return true;
+
+    /**
+     * Set oldid by passed 'snapshot=DATE' query parameter
+     */
+    static function init(&$title, $unused, &$output, &$user, $request, $wiki)
+    {
+        global $wgRequest, $egSnapshot;
+        $egSnapshot = $wgRequest->getVal('snapshot');
+        if ($egSnapshot && $egSnapshot != '1')
+        {
+            $egSnapshot = wfTimestampOrNull(TS_MW, $egSnapshot);
+        }
+        if ($title &&
+            $wgRequest->getVal('action', 'view') == 'view' &&
+            ($egSnapshot && $egSnapshot != '1') &&
+            !$wgRequest->getVal('oldid'))
+        {
+            $wgRequest->setVal('oldid', self::getRevByTime($title, $egSnapshot));
+        }
+        return true;
+    }
+
+    /**
+     * Disable page cache, and optionally set snapshot date from passed oldid
+     * if query parameter 'snapshot' is equal to '1' (&snapshot=1)
+     */
+    static function start($article, &$outputDone, &$useParserCache)
+    {
+        global $wgRequest, $egSnapshot;
+        if ($article->getOldId() && $egSnapshot)
+        {
+            if ($egSnapshot == '1')
+            {
+                $egSnapshot = $article->getTimestamp();
+            }
+            $useParserCache = false;
+        }
+        return true;
+    }
+
+    /**
+     * Override revision of included template
+     */
+    static function templateRev($parser, $title, &$skip, &$id)
+    {
+        global $egSnapshot;
+        if ($egSnapshot)
+        {
+            $id = self::getRevByTime($title, $egSnapshot);
+        }
+        return true;
+    }
+
+    /**
+     * Override revision of included file
+     */
+    static function fileRev($parser, $title, &$time, &$sha1, &$descQuery)
+    {
+        global $egSnapshot;
+        if ($egSnapshot)
+        {
+            $dbr = wfGetDB(DB_SLAVE);
+            $time = $dbr->selectField(
+                'oldimage', 'oi_timestamp', array(
+                    'oi_name' => $title->getDBkey(),
+                    'oi_timestamp <= \''.wfTimestamp(TS_MW, $egSnapshot).'\'',
+                ), __METHOD__, array('ORDER BY' => 'oi_timestamp DESC', 'LIMIT' => 1)
+            );
+        }
+        return true;
+    }
+
+    /**
+     * Include snapshot time into all page links
+     */
+    static function linkRev($dummy, $target, &$html, &$customAttribs, &$query, &$options, &$ret)
+    {
+        global $egSnapshot;
+        if ($egSnapshot)
+        {
+            $query['snapshot'] = $egSnapshot;
+        }
+        return true;
+    }
 }
